e9f1f593bc84699514fec2332df8e9c71e9bb071
amirmohammad <aqasyamyrmhmd@gmail.com>
2025-07-17 00:10:35 +0330

    tree

--- main.cpp
+++ main.cpp
@@ -1,119 +1,113 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
 
-class function {
-public:
-    virtual void add() = 0;
-    virtual void show() = 0;
-};
-
-class Node {
-public:
-    static Node* head; 
-    Node* next = NULL;
-    string name, id,type;
-
-    Node(string name, string id,string type) {
-        this->name = name;
-        this->id = id;
-        this->type = type;
-        next = NULL;
-    }
-    Node() {}
-};
-Node* Node::head = NULL;
-
-class worker : public Node, public function
+class test
 {
-    string name_w, id_w;
-    static int count_w;
-    Node** memory = new Node * [100];  
-
 public:
-    void add() override {
-        cout << "enter your name: ";
-        cin >> name_w;
-        cout << "enter your id: ";
-        cin >> id_w;
+    string name, last_name;
+    test* right, * left, * pre;
 
-        Node* newnode = new Node(name_w, id_w,"worker");
-        memory[count_w] = newnode;
+    test()
+    {
+        right = NULL;
+        left = NULL;
+    }
 
-        if (Node::head == NULL) {
-            Node::head = newnode;
-            count_w++;
-        }
-        else {
-            bool test = false;
-            if (head != NULL && count_w == 0)
-            {
-                count_w++;
-                test = true;
-            }
-            memory[count_w - 1]->next = newnode;
-            if (!test)
-                count_w++;
-        }
-    }
-    void show() override {
-        for (int i = 0; i < count_w; i++) {
-            cout << i + 1 << '-' << memory[i]->type << " " << memory[i]->name << " " << memory[i]->id << endl;
-        }
-    }
+    test* add(test*&, string, string);
+    bool search(test*, string, string);
 };
-int worker::count_w = 0;
+bool connect(test* current, test* newnode)
+{
+    if (current->left == NULL)
+    {
+        current->left = newnode;
+        return true;
+    }
+    else if (current->right == NULL)
+    {
+        current->right = newnode;
+        return true;
+    }
+    if (connect(current->left, newnode))
+        return true;
+    return connect(current->right, newnode);
+}
 
-class employee : public Node, public function {
-    string name_e, id_e;
-    static int count_e;
-    Node** memory = new Node * [100];
-public:
-    void add() override {
-        cout << "enter your name: ";
-        cin >> name_e;
-        cout << "enter id: ";
-        cin >> id_e;
-        Node* newnode = new Node(name_e, id_e,"employee");
-        memory[count_e] = newnode;
-        if (Node::head == NULL) {
-            Node::head = newnode;
-            count_e++;
-        }
-        else {
-            bool test = false;
-            if (head != NULL && count_e == 0)
-            {
-                count_e++;
-                test = true;
-            }
-            memory[count_e - 1]->next = newnode;
-            if(!test)
-            count_e++;
-        }
-    }
-    void show() override {
-        for (int i = 0; i < count_e; i++) {
-            cout << i + 1<<'-' << memory[i]->type << " " << memory[i]->name << " " << memory[i]->id << endl;
-        }
-    }
-};
-int employee::count_e = 0;
+test* test::add(test*& head, string newName, string newLastName)
+{
+    test* newnode = new test();
+    newnode->name = newName;
+    newnode->last_name = newLastName;
 
-int main() {
-    worker w;
-    employee e;
-    int ch;
-    while (true) {
-        cout << "1-add worker|2-add employi|3-show worker|4-show empoloyee|5-exit ";
-        cin >> ch;
-        switch (ch) {
-        case 1: w.add(); break;
-        case 2: e.add(); break;
-        case 3: w.show(); break;
-        case 4: e.show(); break;
-        case 5: return 0;
-        default: cout << "invalid";
+    if (head == NULL)
+    {
+        head = newnode;
+        return head;
+    }
+
+    connect(head, newnode);
+    return head;
+}
+
+bool test::search(test* head, string Name, string Lastname)
+{
+    if (head == NULL)
+        return false;
+
+    if (head->name == Name && head->last_name == Lastname)
+        return true;
+
+    if (head->left && search(head->left, Name, Lastname))
+        return true;
+
+    if (head->right && search(head->right, Name, Lastname))
+        return true;
+
+    return false;
+}
+
+int main()
+{
+    bool start = true;
+    int check;
+    string name, lastName;
+    test* head = NULL;
+
+    while (start)
+    {
+        cout << "1-add | 2-search | 3-exit: ";
+        cin >> check;
+
+        switch (check)
+        {
+        case 1:
+            cout << "Enter name: ";
+            cin >> name;
+            cout << "Enter last name: ";
+            cin >> lastName;
+            head = test().add(head, name, lastName);
+            break;
+
+        case 2:
+            cout << "Enter name: ";
+            cin >> name;
+            cout << "Enter last name: ";
+            cin >> lastName;
+            if (test().search(head, name, lastName))
+                cout << name << " " << lastName << " exists." << endl;
+            else
+                cout << "Not exist." << endl;
+            break;
+
+        case 3:
+            start = false;
+            break;
+
+        default:
+            cout << "Invalid choice!" << endl;
+            break;
         }
     }
+
+    return 0;
 }
